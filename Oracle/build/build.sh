a06be7921fb989c2ef0345a2c9f81025256c39ef
manish <cusat.manish07@gmail.com>
2020-03-24 20:46:45 +0530

    Build file modified

--- Oracle/build/build.sh
+++ Oracle/build/build.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
-
+/Users/manishroy/Manish
 wrk_spc=/Users/manishroy/.jenkins/workspace/MyJOB
-dep_dir=/u02
+home_dir=/tmp/Manish
 
-if [ -d $dep_dir ]; then
- rm -rf $dep_dir
+
+if [ -d $home_dir ]; then
+ rm -rf $home_dir
 fi
 
-mkdir $dep_dir
+mkdir -p $home_dir
 
-cp -r $wrk_spc/* /u02/
+cp -r $wrk_spc/* $home_dir
 chmod -R 755 /u02
 
 #Execution of script 
 
-sh /u02/Oracle/shell/add.sh
-sh /u02/Oracle/shell/add1.sh 
-sh /u02/Oracle/shell/mult.sh
-sh /u02/Oracle/shell/mult1.sh
+sh $home_dir/Oracle/shell/add.sh
+sh $home_dir/Oracle/shell/add1.sh 
+sh $home_dir/Oracle/shell/mult.sh
+sh $home_dir/Oracle/shell/mult1.sh
 
-sh /u02/Oracle/hello.sh
-sh /u02/Oracle/hellworld.sh
-sh /u02/Oracle/hellworld1.sh
+sh $home_dir/Oracle/hello.sh
+sh $home_dir/Oracle/hellworld.sh
+sh $home_dir/Oracle/hellworld1.sh
 
 
 echo "Build Is Successful" 
