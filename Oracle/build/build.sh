12d92317559ee1bbeaf9418f4c4cb730f4ad26e6
manish <cusat.manish07@gmail.com>
2020-03-27 23:20:59 +0530

    build file modified

--- Oracle/build/build.sh
+++ Oracle/build/build.sh
@@ -5,13 +5,14 @@ trg_dir=/u02
 
 
 if [ -d $trg_dir ]; then
- rm -rf $trg_dir
+ chown -R jenkins:jenkins $trg_dir
+ rm -rf $trg_dir/*
 fi
 
 mkdir -p $trg_dir
 chmod -R 755 $trg_dir
-chown -R jenkins:jenkins $trg_dir  
 cp -r $wrk_spc/* $trg_dir
+chown -R jenkins:jenkins $trg_dir  
 
 #Execution of script 
 
